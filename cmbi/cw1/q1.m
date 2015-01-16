21bec9848351a43a67dd2da16c51f3edac128a9e
Razvan Valentin Marinescu <mrazvan22@gmail.com>
2015-01-16 18:03:10 +0000

    started q114

--- cmbi/cw1/q1.m
+++ cmbi/cw1/q1.m
@@ -8,8 +8,9 @@ H = 112;
 [dwis, qhat, bvals] = preprocessing(NR_IMAGES, W,H);
 
 %q111(dwis, qhat, bvals);
-q112(dwis, qhat, bvals);
+%q112(dwis, qhat, bvals);
 %q113(dwis, qhat, bvals);
+q114(dwis, qhat, bvals);
 %q116(dwis, qhat, bvals);
 end
 
@@ -91,23 +92,34 @@ end
 
 function q113(dwis, qhat, bvals)
 
-Avox = dwis(:,52,62,25);
+%Avox = dwis(:,52,62,25); given voxel
+%Avox = dwis(:,23,40,18);
+Avox = dwis(:,70,64,14);
+
+[parameter_hat, minSSD] = fitVoxGlob1(Avox, qhat, bvals);
+
+h = eyeball(Avox, parameter_hat, bvals, qhat);
+
+end
+
+function [parameter_hat, minSSD] = fitVoxGlob1(Avox, qhat, bvals)
+
 % apply the inverse tranformations: sqrt and tangent
 startx = [sqrt(7.5e+05) sqrt(3e-03) q1TransInv(2.5e-01) 0 0];
 
-NR_ITERATIONS = 100;
+NR_ITERATIONS = 10;
 
 minSSD = inf;
 minCounter = 0;
-tol = 1e-8;
+globTol = 0.1;
 for i=1:NR_ITERATIONS
-    i
+    
     sigma = eye(5);
     sigma(1,1) = sqrt(7.5e+05);
     sigma(2,2) = sqrt(3e-03);
     sigma(3,3) = 5;
-    sigma(4,4) = 2*pi;
-    sigma(5,5) = 2*pi;
+    sigma(4,4) = pi;
+    sigma(5,5) = pi;
 
     deltaX = mvnrnd(zeros(1,5),sigma);
     newStartX = startx + deltaX;
@@ -122,17 +134,14 @@ for i=1:NR_ITERATIONS
     [parameter_hat, RESNOM, EXITFLAG, OUTPUT] = fminunc('BallStickSSDq112', newStartX, h, Avox, bvals, qhat);
     %RESNOM
 
-    % apply the transformations
-    [S0 d f theta phi] = deal(parameter_hat(1),parameter_hat(2),parameter_hat(3),parameter_hat(4),parameter_hat(5));
-    parameter_hat = [ S0^2 d^2 atan(f) theta phi];
- 
-    if(abs(minSSD - RESNOM) < tol)
+    parameter_hat(4:5) = mod(parameter_hat(4:5),2*pi);
+    if(abs(minSSD - RESNOM) < globTol && sum(abs(parameter_hat - minParHat)) < globTol )
        % found a previous minimum
        minSSD
        minCounter = minCounter + 1; 
     end
     
-    if (abs(minSSD - RESNOM) > tol && minSSD > RESNOM)
+    if (abs(minSSD - RESNOM) > globTol && minSSD > RESNOM)
         % found a new global minimum
        minSSD = RESNOM;
        minSSD
@@ -146,9 +155,31 @@ end
 parameter_hat = minParHat;
 % apply the transformations
 [S0 d f theta phi] = deal(parameter_hat(1),parameter_hat(2),parameter_hat(3),parameter_hat(4),parameter_hat(5));
-parameter_hat = [ S0^2 d^2 q1Trans(f) theta phi]
+parameter_hat = [ S0^2 d^2 q1Trans(f) theta phi];
 
-h = eyeball(Avox, parameter_hat, bvals, qhat);
+end
+
+function q114(dwis, qhat, bvals)
+
+SLICE_NR=25;
+
+[NR_IMAGES,W,H,~] = size(dwis);
+
+mapS0 = zeros(W,H);
+mapD = zeros(W,H);
+mapF = zeros(W,H);
+mapRESNORM = zeros(W,H);
+
+for w=1:W
+    w
+    for h=1:H   
+        vox = dwis(:,w,h,SLICE_NR);
+        [parameter_hat, mapRESNORM(w,h)] = fitVoxGlob1(vox, qhat, bvals);
+        [mapS0(w,h), mapD(w,h), mapF(w,h), theta, phi] = deal(parameter_hat(1),parameter_hat(2),parameter_hat(3),parameter_hat(4),parameter_hat(5));
+        
+    end
+end
+
 
 
 end
@@ -165,7 +196,7 @@ h = optimset('MaxFunEvals', 20000, 'Algorithm', 'interior-point',...
     'TolX', 1e-10, 'TolFun', 1e-10, 'Display', 'iter');
 % S0 d f theta phi
 lb = [0  , 0  , 0, -inf, -inf];
-ub = [inf, inf, 1,  inf, inf ]; 
+ub = [inf, inf, 1,  inf,  inf]; 
 % Now run the fitting
 % RESNOM is the value of the function at the solution found (parameter_hat)
 [parameter_hat, RESNOM, EXITFLAG, OUTPUT] = fmincon('BallStickSSD', startx, [],[],[],[],lb, ub, [], h, Avox, bvals, qhat)
